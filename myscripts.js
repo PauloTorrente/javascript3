544c15b7b8252d5f7de1a4d404c6716e403101ed
Paulo Moreno <96285036+PauloTorrente@users.noreply.github.com>
2024-03-25 08:06:14 +0100

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -58,3 +58,54 @@ console.log(duplicaNumero(7))
 
  }
 console.log(ultimoCaracter("hola"))
+
+function cuentaCaracteres(str) {
+    if (typeof str !== 'string') {
+        return 'Debo ser ejecutada con un string';
+    }
+    return str.length;
+}
+
+function esPalindromo(cadena) {
+    if (typeof cadena !== 'string' || cadena.trim() === '') {
+        return 'no es un formato correcto';
+    }
+    const reversed = cadena.split('').reverse().join('');
+    return cadena === reversed;
+}
+
+function getPrecioMostrar(precio) {
+    if (typeof precio !== 'number') {
+        return 'no es un formato correcto';
+    }
+    return precio.toFixed(2) + ' €';
+}
+
+function division(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'no es un formato correcto';
+    }
+    if (num2 === 0) {
+        return 'No se puede dividir por cero';
+    }
+    return num1 / num2;
+}
+
+function filtrarMayoresCinco(numeros) {
+    if (!Array.isArray(numeros)) {
+        return 'No es un formato correcto';
+    }
+    return numeros.filter(num => num >= 5);
+}
+
+function esPrimo(numero) {
+    if (typeof numero !== 'number' || numero <= 1 || !Number.isInteger(numero)) {
+        return false;
+    }
+    for (let i = 2; i <= Math.sqrt(numero); i++) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
